8b563feeb16860f197d81ecfc14f3400caa20939
azoller11 <azoller11@gmail.com>
2022-01-23 16:43:02 -0700

    Milestone 6 requirements

---
+++ app/Http/Controllers/UsersRestController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+
+class UsersRestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        echo "Hello from the UsersRestController";
+    }
+
+   
+    
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        echo "UsersRestController ID: " . $id;
+    }
+
+   
+   
+}
